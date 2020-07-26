edca7332389f47a93a4f95016b7d8a6a6475a564
Gustavo Parreira <gustavotcparreira@gmail.com>
2020-07-26 17:57:05 +0100

    Removed default template method

--- ios/ShareMenu.m
+++ ios/ShareMenu.m
@@ -4,8 +4,4 @@
 
 RCT_EXTERN_METHOD(getSharedText:(RCTResponseSenderBlock)callback)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-
 @end
--- ios/ShareMenu.swift
+++ ios/ShareMenu.swift
@@ -75,9 +75,4 @@ open class ShareMenu: NSObject {
         
         callback([""])
     }
-
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
-    }
 }
